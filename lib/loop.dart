137c7163c3422e7cdf5aa6609be6e25c0b1eced1
Md Araful islam Raju <143991599+araful39@users.noreply.github.com>
2024-07-02 02:21:54 -0700

    voter and not voter check add

---
+++ lib/loop.dart
@@ -0,0 +1,25 @@
+class Loop{
+
+
+  void loop(){
+  // var n=6;
+  //     for (int i = 1; i <= n; i++) {
+  //       for (int j = 1; j <= i; j++) {
+  //         print("*");
+  //       }
+  //       print("a");
+  //
+  //   }
+
+
+    String a="Md. Araful \nislam "
+        "Raju";
+    print(a);
+
+
+
+  }
+
+
+
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
-import 'package:classic_it_dart_practice/voter_check.dart';
+import 'package:classic_it_dart_practice/loop.dart';
 
 void main(){
 
 // print(VoterCheck.voterCheck());
-VoterCheck.voterCheck();
+// VoterCheck.voterCheck();
+
+var a=Loop();
+a.loop();
 
 }
